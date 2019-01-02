c857c0fe0f3bf4621908ebc02ca6a3735ee7360e
skt-t1-byungi <tiniwz@gmail.com>
2019-01-02 22:03:23 +0900

    upgrade deps & reindent

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,44 @@
 class Deferred<T> {
-  public static default = Deferred
-  public static Deferred = Deferred
+    public static default = Deferred
+    public static Deferred = Deferred
 
-  private _state: 'pending' | 'resolved' | 'rejected' = 'pending'
-  private _promise: Promise<T>
-  private _resolve!: (v: T) => void
-  private _reject!: (reason: any) => void
+    private _state: 'pending' | 'resolved' | 'rejected' = 'pending'
+    private _promise: Promise<T>
+    private _resolve!: (v: T) => void
+    private _reject!: (reason: any) => void
 
-  constructor () {
-    this._promise = new Promise((resolve, reject) => {
-      this._resolve = resolve
-      this._reject = reject
-    })
-  }
+    constructor () {
+        this._promise = new Promise((resolve, reject) => {
+            this._resolve = resolve
+            this._reject = reject
+        })
+    }
 
-  get promise () {
-    return this._promise
-  }
+    get promise () {
+        return this._promise
+    }
 
-  get state () {
-    return this._state
-  }
+    get state () {
+        return this._state
+    }
 
-  get isCompleted () {
-    return this._state !== 'pending'
-  }
+    get isCompleted () {
+        return this._state !== 'pending'
+    }
 
-  public resolve (val: T) {
-    if (this.isCompleted) return
+    public resolve (val: T) {
+        if (this.isCompleted) return
 
-    this._state = 'resolved'
-    this._resolve(val)
-  }
+        this._state = 'resolved'
+        this._resolve(val)
+    }
 
-  public reject (reason: any) {
-    if (this.isCompleted) return
+    public reject (reason: any) {
+        if (this.isCompleted) return
 
-    this._state = 'rejected'
-    this._reject(reason)
-  }
+        this._state = 'rejected'
+        this._reject(reason)
+    }
 }
 
 export = Deferred
--- src/test.ts
+++ src/test.ts
@@ -2,17 +2,17 @@ import test from 'ava'
 import Defer from '.'
 
 test.cb('basic', t => {
-  const defer = new Defer()
+    const defer = new Defer()
 
-  t.is(defer.state, 'pending')
-  t.false(defer.isCompleted)
+    t.is(defer.state, 'pending')
+    t.false(defer.isCompleted)
 
-  defer.promise.then(v => {
-    t.is(v, 'test!')
-    t.is(defer.state, 'resolved')
-    t.true(defer.isCompleted)
-    t.end()
-  })
+    defer.promise.then(v => {
+        t.is(v, 'test!')
+        t.is(defer.state, 'resolved')
+        t.true(defer.isCompleted)
+        t.end()
+    })
 
-  defer.resolve('test!')
+    defer.resolve('test!')
 })
